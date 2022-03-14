fba281058dcd3d4bbe3260633aa1bfaf4c06c626
Davie <webdev.inkshared@gmail.com>
2022-03-14 21:36:53 +0800

    changes on the main design

--- src/components/author.js
+++ src/components/author.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AuthorImg from '../../static/author/dummy-profile.jpg';
 import DummyAuthor from "../../static/author/dummy-profile.jpg";
+import AuthorBackground from "../../static/banner/AuthorBackground.png";
 import { Link } from 'gatsby'
 
 const Author = () => {
@@ -8,26 +9,26 @@ const Author = () => {
         <>
             <section className="upper-main-author" id="main-upper-author">
                 <div className="grid-two-columns">
+                {/* This is a change */}
+                            <div className="grid-child" id="author-contents">
 
-                    <div className="grid-child" id="author-contents">
+                                <p>
+                                A book reader turned a book writer. His stories are a glimpse of his memoirs and his encounters with the pristine nature where he grows up. Looking back on his writing career, he had to say that it is a gift that came from his earliest attempts at capturing an inspiring event that he would feel was in need of being recorded.
+                                </p>
 
-                        <p>
-                        A book reader turned a book writer. His stories are a glimpse of his memoirs and his encounters with the pristine nature where he grows up. Looking back on his writing career, he had to say that it is a gift that came from his earliest attempts at capturing an inspiring event that he would feel was in need of being recorded.
-                        </p>
+                                <h1 className="author-name-tag">
+                                    <span>Brady Herren</span>
+                                </h1>
 
-                        <h1 className="author-name-tag">
-                            <span>Brady Herren</span>
-                        </h1>
+                                <span className="author-tagline">
+                                    Author & Writer
+                                </span>
 
-                        <span className="author-tagline">
-                            Author & Writer
-                        </span>
-
-                        <button >
-                            <Link to="/about-the-author">Read More</Link>
-                        </button>
-                    </div>
-
+                                <button >
+                                    <Link to="/about-the-author">Read More</Link>
+                                </button>
+                            </div>
+                            
                     <div className="grid-child" id="author-image-container">
                         <img alt="author-img" src={AuthorImg} />
                     </div>
